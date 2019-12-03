617c8eaca6e5e06542df47e8853e55936bfea804
Christian Berendt <berendt@betacloud-solutions.de>
2019-12-03 13:25:18 +0100

    Add ceph/daemon latest

--- src/main.py
+++ src/main.py
@@ -18,6 +18,17 @@ TRAVIS_ACCESS_TOKEN = os.getenv('TRAVIS_ACCESS_TOKEN')
 
 # FIXME(berendt): move to YAML file
 RESSOURCES = {
+    'ceph-daemon': {
+        'type': 'docker',
+        'image': 'ceph/daemon',
+        'tag': 'latest',
+        'target': {
+            'image': 'osism/ceph-daemon',
+            'repository': 'docker-ceph-container',
+            'version': 'latest',
+            'parameter': 'VERSION'
+        }
+    },
     'ceph-luminous': {
         'type': 'git',
         'repository': 'ceph/ceph-ansible',
@@ -102,20 +113,63 @@ def trigger_build(repository, branch, message):
     }
     target_parameter = RESSOURCES[repository]['target']['parameter']
     target_version = RESSOURCES[repository]['target']['version']
+
+    if RESSOURCES[repository]['type'] == 'git':
+        env = {
+            'global': [
+                "%s=%s" % (target_parameter, target_version)
+            ]
+        }
+    elif RESSOURCES[repository]['type'] == 'docker':
+        env = {
+            'global': [
+                "%s=%s" % (target_parameter, target_version),
+                "REPOSITORY=%s" % RESSOURCES[repository]['target']['image']
+            ]
+        }
+
     json = {
         'request': {
             'branch': 'master',
             'message': message,
-            "config": {
-                "env": {
-                    "global": ["%s=%s" % (target_parameter, target_version)]
-                }
+            'config': {
+                'env': env
             }
         }
     }
 
     response = requests.post(url, headers=headers, json=json)
 
+
+def check_image(image, tag):
+    mc = Minio(MINIO_SERVER, access_key=MINIO_ACCESS_KEY, secret_key=MINIO_SECRET_KEY, secure=True)
+
+    try:
+        mc.make_bucket("trigger")
+    except:
+        pass
+
+    try:
+        last_updated = mc.get_object('trigger', "%s/updated" % image)
+        last_updated = datetime.strptime(last_updated.data.decode('ascii'), '%Y-%m-%dT%H:%M:%S.%fZ')
+        last_updated = pytz.utc.localize(last_updated)
+    except:
+        last_updated = datetime(1970, 1, 1, tzinfo=pytz.UTC)
+
+    url = "https://hub.docker.com/v2/repositories/%s/tags/%s" % (RESSOURCES[image]['image'], tag)
+    response = requests.get(url)
+    updated_str = response.json()['last_updated']
+    updated = datetime.strptime(updated_str, '%Y-%m-%dT%H:%M:%S.%fZ')
+    updated = pytz.utc.localize(updated)
+
+    if updated > last_updated:
+        message = "%s (%s)" % (RESSOURCES[image]['image'], tag)
+        trigger_build(image, tag, message)
+
+    try:
+        mc.put_object('trigger', "%s/updated" % image, io.BytesIO(updated_str.encode('utf-8')), len(updated_str.encode('utf-8')))
+    except:
+        pass
 
 def check_repository(repository, branch):
     mc = Minio(MINIO_SERVER, access_key=MINIO_ACCESS_KEY, secret_key=MINIO_SECRET_KEY, secure=True)
@@ -155,8 +209,11 @@ def main():
     for ressource in RESSOURCES:
 
         if RESSOURCES[ressource]['type'] == 'git':
-            print("Checking %s @ %s" % (RESSOURCES[ressource]['branch'], RESSOURCES[ressource]['repository']))
+            print("Checking repository %s @ %s" % (RESSOURCES[ressource]['branch'], RESSOURCES[ressource]['repository']))
             check_repository(ressource, RESSOURCES[ressource]['branch'])
+        elif RESSOURCES[ressource]['type'] == 'docker':
+            print("Checking image %s @ %s" % (RESSOURCES[ressource]['tag'], RESSOURCES[ressource]['image']))
+            check_image(ressource, RESSOURCES[ressource]['tag'])
 
 
 if __name__ == '__main__':
